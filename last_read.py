ea45cc9c7fa5162fb32c34ad65ecce15e5e7ed87
Gleison <gleisonmorais06@gmail.com>
2025-03-15 14:21:34 -1200

    Update: Archives and Folders

---
+++ last_read.py
@@ -0,0 +1,5 @@
+class Get_Last_Manga_Read():
+    def __init__(self):
+        pass
+
+    
--- scraper.py
+++ scraper.py
@@ -29,24 +29,42 @@ def get_latest_manga(site_config):
 
         container = site_config['container']
         element = site_config['selector']
-        div_container = soup.select_one(f'.{container[1].replace(" ", ".").replace(":", "\\:")}')
+
+        container_tag = container[0]
+        container_class = container[1].replace(' ', '.').replace(':', '\\:')
+        container_selector = f'{container_tag}.{container_class}'
+        div_container = soup.select_one(container_selector)
         
         if not div_container:
             return None
         
-        element_inside_container = div_container.find_all(element[0], class_=element[1])
+        element_inside_container = []
+        
+        if len(element) == 2:
+            elements = div_container.find_all(element[0], class_=element[1])
+            element_inside_container = elements
+
+        elif len(element) == 3:
+            parent_elements = div_container.find_all(element[0], class_=element[1])
+            for parent in parent_elements:
+                children = parent.find_all(element[2])
+                element_inside_container.extend(children)
+        
         results = []
         for elem in element_inside_container:
-            if link := elem.find('a'):
+            link = elem if elem.name == 'a' else elem.find('a')
+            if link:
                 title = link.get_text(strip=True)
                 url = urljoin(site_config['url'], link.get('href'))
                 results.append({"Title": title, "Url": url, "Time": current_date})
+        
         return results[:1] if results else None
 
     elif site_config['type'] == 'xpath':
         tree = html.fromstring(response.text)
         elements = tree.xpath(site_config['selector'])
         return elements[0].text.strip() if elements else None
+    
 
 def check_updates():
     try:
